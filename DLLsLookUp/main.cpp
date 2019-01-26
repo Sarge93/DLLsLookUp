31e267ce6dd8a2e57873f0747f16d691d8c90c2c
Sarge93 <sarge93@yandex.ru>
2019-01-26 12:42:36 +0300

    add maintaining of address

--- DLLsLookUp/main.cpp
+++ DLLsLookUp/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <windows.h>
 #include <tlhelp32.h>
-#include "main.h"
+#include <psapi.h>
 
 using namespace std;
+
+VOID GetProcessInfo(DWORD CONST dwProcessID) {
+
+	HMODULE hMods[1024];
+	DWORD cbNeeded;
+
+	HANDLE CONST hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessID);
+	if (hProcess == NULL) {
+		cout << "OpenProcess --- " << "LastErrorCode: " << GetLastError() << endl;
+		return;
+	}
+	else {
+		cout << "HOREY" << endl;
+	}
+	if (EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded)) {
+		for (int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++) {
+			TCHAR szModName[MAX_PATH];
+			if (GetModuleFileNameEx(hProcess, hMods[i], szModName, sizeof(szModName) / sizeof(TCHAR))) {
+				cout << szModName << "   " << hMods[i] << endl;
+			}
+		}
+	}
+	else {
+		cout  << "EnumProcessModules --- " << "LastErrorCode: " << GetLastError() << endl;
+		return;
+	}
+}
 
 VOID PrintModuleList(HANDLE CONST hStdOut, DWORD CONST dwProcessID) {
 	MODULEENTRY32 meModuleEntry;
@@ -11,7 +38,6 @@ VOID PrintModuleList(HANDLE CONST hStdOut, DWORD CONST dwProcessID) {
 	if (hSnapshot == INVALID_HANDLE_VALUE) {
 		cout << "LastErrorCode: " << GetLastError() << endl;
 		return;
-		
 	}
 	meModuleEntry.dwSize = sizeof(MODULEENTRY32);
 	Module32First(hSnapshot, &meModuleEntry);
@@ -32,7 +58,7 @@ VOID PrintProcessList(HANDLE CONST hStdOut) {
 	DWORD check;
 	do {
 		cout << i << "   " << peProcessEntry.th32ProcessID << "  " << peProcessEntry.szExeFile << endl;
-		if (i == 43) {
+		if (i == 40) { 
 			check = peProcessEntry.th32ProcessID;
 		}
 		i++;
@@ -42,7 +68,9 @@ VOID PrintProcessList(HANDLE CONST hStdOut) {
 //	cout << "Choose PID" << endl;
 //	cin >> choosingPID;
 	//PrintModuleList(hStdOut, choosingPID);
-	PrintModuleList(hStdOut, check);
+	//PrintModuleList(hStdOut, check);
+	cout << "PID: " << check << endl;
+	GetProcessInfo(check);
 	CloseHandle(hSnapshot);
 }
 
